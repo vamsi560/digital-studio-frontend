2294c16978d4cb81956a4eb0aab2172437c69213
vamsi560 <145781997+vamsi560@users.noreply.github.com>
2025-07-15 14:57:21 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    basicSsl()
+  ],
+  server: {
+    https: true,
+    port: 5173,
+    open: true,  // Automatically open the browser on server start
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
+  }
+})
